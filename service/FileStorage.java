e0f61d99d209c5e4c24a9e5ad7494bbbd64709fd
MaynovaSimamora <maynovasmr@gmail.com>
2025-05-06 21:29:27 +0800

    Menambahkan FileStronge

--- service/FileStorage.java
+++ service/FileStorage.java
@@ -1,5 +1,16 @@
 package service;
 
-public class FileStorage {
-    
+import model.Task;
+import java.io.*;
+import java.util.List;
+
+public abstract class FileStorage {
+    // Abstract method untuk polymorphism
+    public abstract void saveToFile(List<Task> tasks, String filename) throws IOException;
+
+    public List<Task> loadFromFile(String filename) throws Exception {
+        // Implementasi baca file
+        return new ArrayList<>();
+    }
 }
+
