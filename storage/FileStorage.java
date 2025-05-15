ce4fc4ce45f97714fd48f36a4f45c1b8ac77b04c
MaynovaSimamora <maynovasmr@gmail.com>
2025-05-16 03:48:05 +0800

    Perubahan struktur file

--- storage/FileStorage.java
+++ storage/FileStorage.java
@@ -1,16 +1,3 @@
-package storage;
 
-import model.Task;
-import java.io.*;
-import java.util.List;
 
-public abstract class FileStorage {
-    // Abstract method untuk polymorphism
-    public abstract void saveToFile(List<Task> tasks, String filename) throws IOException;
-
-    public List<Task> loadFromFile(String filename) throws Exception {
-        // Implementasi baca file
-        return new ArrayList<>();
-    }
-}
 
