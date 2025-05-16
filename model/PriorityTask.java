28f23eff5adc5812a784d219d819ceb6eb7686e8
annisa <annisahdiani24@gmail.com>
2025-05-16 17:10:32 +0800

    Menambahakan priority dan task

--- model/PriorityTask.java
+++ model/PriorityTask.java
@@ -1,3 +1,22 @@
-public class PriorityTask {
-    
+package model;
+
+import java.time.LocalDate;
+
+public class PriorityTask extends Task {
+    private String priority;
+
+    public PriorityTask(String title, LocalDate dueDate, String priority) {
+        super(title, dueDate);
+        this.priority = priority;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("  ⚠ Prioritas: " + priority);
+    }
 }
